82a394931a881c602ee58b1c191386930721b358
Mary Wong <pair+mwong@modcloth.com>
2013-02-01 22:12:38 -0800

    converted to instance methods in preparation for accumulator instance variable

--- lib/rpn.rb
+++ lib/rpn.rb
@@ -1,22 +1,18 @@
 class RPN
 
-  def self.evaluate(*args)
+  def evaluate(*args)
     op_index = operator_index(args)
     operate(args, op_index)
   end
 
-  def self.operator_index(args)
+  def operator_index(args)
     ['+', '-', '*', '/', '%', '**', '<', '<=>'].each do |op|
       index = args.index(op)
       return index unless index.nil?
     end
   end
 
-  def self.operate(args, op_index)
+  def operate(args, op_index)
     args[op_index-2].to_f.send args[op_index], args[op_index-1]
-  end
-
-  def foo
-    1 + 2
   end
 end
--- spec/rpn_spec.rb
+++ spec/rpn_spec.rb
@@ -3,51 +3,51 @@ require 'rpn'
 describe RPN do
   describe '::evaluate' do
     it 'returns correct result for single +' do
-      subject.class.evaluate(3, 2, '+').should == 5
+      subject.evaluate(3, 2, '+').should == 5
     end
 
     it 'returns correct result for single -' do
-      subject.class.evaluate(3, 2, '-').should == 1
+      subject.evaluate(3, 2, '-').should == 1
     end
 
     it 'returns correct result for single *' do
-      subject.class.evaluate(3, 2, '*').should == 6
+      subject.evaluate(3, 2, '*').should == 6
     end
 
     it 'returns correct result for single / of integers' do
-      subject.class.evaluate(3, 2, '/').should == 1.5
+      subject.evaluate(3, 2, '/').should == 1.5
     end
 
     it 'returns correct result for single / of floats' do
-      subject.class.evaluate(3.0, 2.0, '/').should == 1.5
+      subject.evaluate(3.0, 2.0, '/').should == 1.5
     end
 
     it 'returns correct result for single %' do
-      subject.class.evaluate(3, 2, '%').should == 1
+      subject.evaluate(3, 2, '%').should == 1
     end
 
     it 'returns correct result for single **' do
-      subject.class.evaluate(3, 2, '**').should == 9
+      subject.evaluate(3, 2, '**').should == 9
     end
 
     it 'returns correct result for single <' do
-      subject.class.evaluate(3, 2, '<').should be_false
+      subject.evaluate(3, 2, '<').should be_false
     end
 
     it 'returns correct result for single <=>' do
-      subject.class.evaluate(3, 2, '<=>').should == 1
+      subject.evaluate(3, 2, '<=>').should == 1
     end
   end
 
   describe '::operate_index' do
     it '::operate_index' do
-      RPN.operator_index([3, 2, '+']).should == 2
+      subject.operator_index([3, 2, '+']).should == 2
     end
   end
 
   describe '::operate' do
     it '::operate' do
-      RPN.operate([3, 2, '+'], 2).should == 5
+      subject.operate([3, 2, '+'], 2).should == 5
     end
   end
 end
